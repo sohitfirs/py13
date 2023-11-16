7e9a59d095b3db6c3f14fd51cf44d300c80ada2f
sohitfirs <126562924+sohitfirs@users.noreply.github.com>
2023-11-16 15:52:56 +0300

    Add files via upload

---
+++ py13.py
@@ -0,0 +1,48 @@
+# 1
+
+class Transport:
+
+   def __init__(self, name, max_speed, mileage):
+        self.name = name
+        self.max_speed = max_speed
+        self.mileage = mileage
+    
+class Authobus(Transport):
+    def __init__(self, name, max_speed, mileage):
+       super().__init__(name, max_speed, mileage)
+       
+    def a_print(self):
+        print(f'Name transport: {self.name}, speed: {self.max_speed}, mileage: {self.mileage}')
+
+abobus = Authobus('Renaul Logan', 180, 12)
+
+abobus.a_print()
+print()
+
+# 2
+
+class Transport:
+
+   def __init__(self, name, max_speed, mileage):
+        self.name = name
+        self.max_speed = max_speed
+        self.mileage = mileage
+        
+   def seating_capacity(self, capacity):
+       return f'Capacity of one bus {self.name}: is {capacity} passengers'
+   
+class Authobus(Transport):
+    
+    seating_capacity_ = 50
+
+    def __init__(self, name, max_speed, mileage):
+        super().__init__(name, max_speed, mileage)
+    
+    def seating_capacity(self):
+        return super().seating_capacity(self.seating_capacity_)    
+
+bbb = Transport('Renaul Logan', 200, 100)
+aaa = Authobus('Abobus', 100, 12)
+
+print(bbb.seating_capacity(5))
+print(aaa.seating_capacity())
